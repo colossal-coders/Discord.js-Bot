55233802a1544ccdf06f66618411b9ca19abfa84
Laggy <43407332+LaggyBoi56@users.noreply.github.com>
2019-03-28 12:39:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Prefix = "$:"
+const Token = "Token Here"
 
-//Discord "On Ready" Sub Process
+//Discord "On Ready" Function
 bot.on("ready", function() {
 	console.log("Successfully Connected to Discord, Bot is Ready!");
 });
+
+//Discord "On Message" Sub Process
+bot.on("message", function(message) {
+	if (message.author.equals(bot.user)) return;
+	if (!message.content.startsWith(Prefix)) return;
+	var args = message.content.substring(Prefix.length).split(" ");
+	switch (args[0].toLowerCase()) {
+		case "Test":
+			message.channel.send("Hello World!");
+		break;
+	}
+});
+
+bot.login(Token);
