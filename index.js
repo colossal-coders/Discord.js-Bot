7c0340b57e026eb8813c79d63b27aca35d74bcaa
Laggy <43407332+LaggyBoi56@users.noreply.github.com>
2019-03-28 12:34:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Prefix = "$:"
+
+//Discord "On Ready" Sub Process
+bot.on("ready", function() {
+	console.log("Successfully Connected to Discord, Bot is Ready!");
+});
